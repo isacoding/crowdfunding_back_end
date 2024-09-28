25dd1aae2303851dd9d32927035883b5510974ea
Isabela <isabela.iar01@gmail.com>
2024-09-28 10:58:21 +1000

    Custom auth token added to users/views

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -6,6 +6,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -26,7 +28,26 @@ class CustomUserList(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )   
+    
+    
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
         )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
+    
+
         
 class CustomUserDetail(APIView):
     def get_object(self, pk):
